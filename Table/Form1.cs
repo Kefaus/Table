71aee8cfacc6d268fb570ff3eeeb6a55e235b77a
Kefaus <kirill.sibirtsev.96@mail.ru>
2022-12-21 22:26:03 +0300

    First commit

---
+++ Table/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Table
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlConnection = null;
+
+        private SqlCommandBuilder sqlBuilder = null;
+
+        private SqlDataAdapter sqlDataAdapter = null;
+
+        private DataSet dataSet = null;
+
+        private bool newRowAdding = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                sqlDataAdapter = new SqlDataAdapter("SELECT *, 'Delete' AS [Delete] FROM Users", sqlConnection);
+
+                sqlBuilder = new SqlCommandBuilder(sqlDataAdapter);
+
+                sqlBuilder.GetInsertCommand();
+                sqlBuilder.GetUpdateCommand();
+                sqlBuilder.GetDeleteCommand();
+
+                dataSet = new DataSet();
+
+                sqlDataAdapter.Fill(dataSet, "Users");
+
+                dataGridView1.DataSource = dataSet.Tables["Users"];
+
+                for (int i = 0; i <dataGridView1.Rows.Count; i++)
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridView1[6, i] = linkCell;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ReloadData()
+        {
+            try
+            {
+                dataSet.Tables["Users"].Clear();
+
+                sqlDataAdapter.Fill(dataSet, "Users");
+
+                dataGridView1.DataSource = dataSet.Tables["Users"];
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridView1[6, i] = linkCell;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=H:\Table1\Table\Database1.mdf;Integrated Security=True");
+
+            sqlConnection.Open();
+
+
+            LoadData();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            ReloadData();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.ColumnIndex == 6)
+                {
+                    string task = dataGridView1.Rows[e.RowIndex].Cells[6].Value.ToString();
+
+                    if (task == "Delete")
+                    {
+                        if (MessageBox.Show("Удалить эту строку?", "Удаление", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+                            == DialogResult.Yes)
+                        {
+                            int rowIndex = e.RowIndex;
+
+                            dataGridView1.Rows.RemoveAt(rowIndex);
+
+                            dataSet.Tables["Users"].Rows[rowIndex].Delete();
+
+                            sqlDataAdapter.Update(dataSet, "Users");
+                        }
+                    }
+                    else if (task == "Insert")
+                    {
+                        int rowIndex = dataGridView1.Rows.Count - 2;
+
+                        DataRow row = dataSet.Tables["Users"].NewRow();
+
+                        row["Студент"] = dataGridView1.Rows[rowIndex].Cells["Студент"].Value;
+                        row["Английский"] = dataGridView1.Rows[rowIndex].Cells["Английский"].Value;
+                        row["Программирование"] = dataGridView1.Rows[rowIndex].Cells["Программирование"].Value;
+                        row["Математика"] = dataGridView1.Rows[rowIndex].Cells["Математика"].Value;
+                        row["Русский"] = dataGridView1.Rows[rowIndex].Cells["Русский"].Value;
+
+                        dataSet.Tables["Users"].Rows.Add(row);
+
+                        dataSet.Tables["Users"].Rows.RemoveAt(dataSet.Tables["Users"].Rows.Count - 1);
+
+                        dataGridView1.Rows.RemoveAt(dataGridView1.Rows.Count - 2);
+
+                        dataGridView1.Rows[e.RowIndex].Cells[6].Value = "Delete";
+
+                        sqlDataAdapter.Update(dataSet, "Users");
+
+                        newRowAdding = false;
+                    }
+                    else if (task == "Update")
+                    {
+                        int r = e.RowIndex;
+
+                        dataSet.Tables["Users"].Rows[r]["Студент"] = dataGridView1.Rows[r].Cells["Студент"].Value;
+                        dataSet.Tables["Users"].Rows[r]["Английский"] = dataGridView1.Rows[r].Cells["Английский"].Value;
+                        dataSet.Tables["Users"].Rows[r]["Программирование"] = dataGridView1.Rows[r].Cells["Программирование"].Value;
+                        dataSet.Tables["Users"].Rows[r]["Математика"] = dataGridView1.Rows[r].Cells["Математика"].Value;
+                        dataSet.Tables["Users"].Rows[r]["Русский"] = dataGridView1.Rows[r].Cells["Русский"].Value;
+
+                        sqlDataAdapter.Update(dataSet, "Users");
+
+                        dataGridView1.Rows[e.RowIndex].Cells[6].Value = "Delete";
+                    }
+
+                    ReloadData();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_UserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            try
+            {
+                if (newRowAdding == false)
+                {
+                    newRowAdding = true;
+
+                    int lastRow = dataGridView1.Rows.Count - 2;
+
+                    DataGridViewRow row = dataGridView1.Rows[lastRow];
+
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridView1[6, lastRow] = linkCell;
+
+                    row.Cells["Delete"].Value = "Insert";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (newRowAdding == false)
+                {
+                    int rowIndex = dataGridView1.SelectedCells[0].RowIndex;
+
+                    DataGridViewRow editingRow = dataGridView1.Rows[rowIndex];
+
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridView1[6, rowIndex] = linkCell;
+
+                    editingRow.Cells["Delete"].Value = "Update";
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            e.Control.KeyPress -= new KeyPressEventHandler(Column_KeyPress);
+
+            if (dataGridView1.CurrentCell.ColumnIndex == 2)
+            {
+                TextBox textBox = e.Control as TextBox;
+
+                if (textBox != null)
+                {
+                    textBox.KeyPress += new KeyPressEventHandler(Column_KeyPress);
+                }
+            }
+
+            if (dataGridView1.CurrentCell.ColumnIndex == 3)
+            {
+                TextBox textBox = e.Control as TextBox;
+
+                if (textBox != null)
+                {
+                    textBox.KeyPress += new KeyPressEventHandler(Column_KeyPress);
+                }
+            }
+
+            if (dataGridView1.CurrentCell.ColumnIndex == 4)
+            {
+                TextBox textBox = e.Control as TextBox;
+
+                if (textBox != null)
+                {
+                    textBox.KeyPress += new KeyPressEventHandler(Column_KeyPress);
+                }
+            }
+
+            if (dataGridView1.CurrentCell.ColumnIndex == 5)
+            {
+                TextBox textBox = e.Control as TextBox;
+
+                if (textBox != null)
+                {
+                    textBox.KeyPress += new KeyPressEventHandler(Column_KeyPress);
+                }
+            }
+        }
+
+        private void Column_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void export_Click(object sender, EventArgs e)
+        {
+            Excel.Application exApp = new Excel.Application();
+
+            exApp.Workbooks.Add();
+            Excel.Worksheet wsh = (Excel.Worksheet)exApp.ActiveSheet;
+            int i, j;
+            for (i = 0; i <= dataGridView1.RowCount - 2; i++)
+            {
+                for (j = 0; j <= dataGridView1.ColumnCount - 1; j++)
+                {
+                    wsh.Cells[1, j + 1] = dataGridView1.Columns[j].HeaderText.ToString();
+                    wsh.Cells[i + 2, j + 1] = dataGridView1[j, i].Value.ToString();
+
+                }
+            }
+
+            exApp.Visible = true;
+        }
+
+    }
+}
